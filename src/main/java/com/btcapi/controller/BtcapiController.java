0067727c35717090daae86c7b51ed5ff96a2c68d
vitorhugoze <vitorhugozeferino@hotmail.com>
2026-05-24 20:28:06 -0300

    Adicionado endpoint

--- src/main/java/com/btcapi/controller/BtcapiController.java
+++ src/main/java/com/btcapi/controller/BtcapiController.java
@@ -19,6 +19,10 @@ import com.btcapi.util.CsvPriceReader;
 @RestController
 public class BtcapiController {
 
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("teste!");
+    }
+
     @GetMapping("/prices")
     public ResponseEntity<List<PriceRecord>> getBtcPrices(
             @RequestHeader(value = "iniDate", required = false) String iniDate,
