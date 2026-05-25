908931db3e01c5af27806cbe120e99eef2acd7f4
vitorhugoze <vitorhugozeferino@hotmail.com>
2026-05-25 10:45:50 -0300

    limpeza de imports

--- src/main/java/com/btcapi/controller/BtcapiController.java
+++ src/main/java/com/btcapi/controller/BtcapiController.java
@@ -2,8 +2,6 @@ package com.btcapi.controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
